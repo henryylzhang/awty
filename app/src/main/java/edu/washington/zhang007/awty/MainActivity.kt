22e9c327bfeffaceaa553f271886f62554858d79
Henry Zhang <henry.zhang@live.com>
2018-05-15 15:13:46 -0700

    Add permissions request for API 23 and above

--- app/src/main/java/edu/washington/zhang007/awty/MainActivity.kt
+++ app/src/main/java/edu/washington/zhang007/awty/MainActivity.kt
@@ -1,21 +1,40 @@
 package edu.washington.zhang007.awty
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.util.Log
+
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    private var MY_PERMISSIONS_REQUEST_SEND_SMS = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // from Google Developer Documentation
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            Log.d(TAG, "PERMISSION TO SEND SMS NOT GRANTED!")
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.SEND_SMS),
+                    MY_PERMISSIONS_REQUEST_SEND_SMS)
+        }
+        // end from Google Developer Documentation
 
         // UI Declarations
         val msg = editText_message.text
@@ -71,4 +90,28 @@ class MainActivity : AppCompatActivity() {
 
         return "($areaCode) $prefix-$lineNumber"
     }
+
+    // from https://stackoverflow.com/questions/33666071/android-marshmallow-request-permission
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this@MainActivity, "Permission denied to send SMS", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }// other 'case' lines to check for other
+        // permissions this app might request
+    }
+    // end from Stack Overflow
 }
